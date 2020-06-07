d6514908730bd3c07815e8334523acdcac064961
Adam Emieljaniuk <beginner1986@gmail.com>
2020-06-07 22:25:16 +0200

    Missed SQL files uploaded.

--- AwirMvc/src/main/resources/data.sql
+++ AwirMvc/src/main/resources/data.sql
@@ -1,12 +1,3 @@
-DROP TABLE IF EXISTS user;
-
-CREATE TABLE user (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(250) NOT NULL,
-  last_name VARCHAR(250) NOT NULL,
-  email VARCHAR(250) DEFAULT NULL
-);
- 
 INSERT INTO user (first_name, last_name, email) VALUES
   ('Jan', 'Kowalski', 'j.kowalski@gmail.com'),
   ('Maria', 'Kowalska', 'maria.kowalska@wp.pl'),
